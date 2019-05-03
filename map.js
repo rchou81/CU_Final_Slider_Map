e90045f7f2d599b10f8f34239a931ed6c11dbd44
Richard <richardchou81@gmail.com>
2019-05-03 01:39:11 -0400

    update

--- map.js
+++ map.js
@@ -7,16 +7,16 @@ console.log('Loaded map.js')
 mapboxgl.accessToken = 'pk.eyJ1IjoicmNob3U4MSIsImEiOiJjanNjcG43Z2UwNGRwNDNwZWN0NTB0MW04In0.L5P48cRwORYjZkyPJ44quQ'
 
 let map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/rchou81/cjv7n19rw1rp31gp3n7uen9fl',
-	center: [-66.412985,18.233759],
-	zoom: 7,
-	pitch: 0
+    container: 'map',
+    style: 'mapbox://styles/rchou81/cjv6xqqdr0c751fpmfk1pk1vr',
+    center: [-66.412985,18.233759],
+    zoom: 9,
+    pitch: 0
 })
 
 //create an instance of NavigationControl
 let navigation = new mapboxgl.NavigationControl({
-	showCompass: true
+    showCompass: true
 })
 
 // add the navigation to your map
@@ -24,8 +24,8 @@ map.addControl(navigation, 'top-left')
 
 // create an instance of ScaleControl
 let scale = new mapboxgl.ScaleControl({
-	maxWidth: 80,
-	unit: 'imperial'
+    maxWidth: 80,
+    unit: 'imperial'
 })
 
 
@@ -33,25 +33,36 @@ let scale = new mapboxgl.ScaleControl({
 map.addControl(scale, 'bottom-right')
 
 let geolocate = new mapboxgl.GeolocateControl({
-	potisionOptions: {
-		enableHighAvvurary: true
-	},
-	trackUserLocation: true,
-	showUserLocation: true,
-	fitBoundsOptions: {
-	}
+    potisionOptions: {
+        enableHighAvvurary: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
 })
 
 map.addControl(geolocate, 'top-left')
 
 //this is an event handler
 geolocate.on('geolocate', function(event) {
-	let lng = event.coords.longitude
-	let lat = event.coords.latitude
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
 
-	//debug
-	console.log('geolocated:', lng, lat)
+    //debug
+    console.log('geolocated:', lng, lat)
 
-	// format lng lat values and display them on our 'info' element
-	document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)	
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)   
+})
+
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
 })
