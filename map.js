4224c327b844326aa3d5174cd8ecc0c0858fd8dc
Richard <richardchou81@gmail.com>
2019-05-03 01:35:30 -0400

    Update map.js

--- map.js
+++ map.js
@@ -68,37 +68,4 @@ map.on('click', function(event) {
 })
 
 
-let data = [
-    {
-        location: [-73.961786,40.798958],
-        content: 'Osteria 106<br/><img src="https://lh5.googleusercontent.com/p/AF1QipMfWN8IHehAn28lv0XR-SHePvNLljX13m4lY96a=w408-h306-k-no" />'
-    },
-    {
-        location: [ -73.964300,40.802977],
-        content: 'Marlow Bistro<br /><img src="https://lh4.googleusercontent.com/proxy/Ae1OAN807SAJuxO0smr_7jx5Q8D3I6tMzuj0IERYAwTANeE1DNYT9CwxiZAUlEuDF1OQ8TwmrHwrWTZiktd_gVfYE9VuFNZTeqvD9rYaPsymSdPi9BidOSodYfjzCtm2bST5LIECDvKqL7GWsTcSorZqXQeZPg=w408-h408-k-no" />'
-   	}, 
-    {
-        location: [ -73.965829,40.800661],
-        content: 'The Ellington<br /><img src="https://lh5.googleusercontent.com/p/AF1QipNUgFuXB_-CWod6ViTggDZyhELg7n5TLoXXNYPU=w408-h229-k-no" />'    
-    },
-    {
-        location: [ -73.965891,40.805801],
-        content: 'Le Monde<br /><img src="https://lh5.googleusercontent.com/p/AF1QipPLe0Ap70DD6YpoMVa8KDBRYPsFKwSfUVT1khG7=w408-h544-k-no" />'    
-    },
-    {
-        location: [  -73.963779, 40.803488],
-        content: 'Hula Poke<br /><img src="https://lh5.googleusercontent.com/p/AF1QipNoy3fLFuiyHPMoT2g0BiBCjyOp3-cytJkizeB5=w408-h306-k-no" />'    
-    },
-    ]
-
-    data.forEach(function(d) {
-
-    let marker = new mapboxgl.Marker( {color: 'red'})    
-    marker.setLngLat(d.location)
-    marker.addTo(map)  
-
-    let popup = new mapboxgl.Popup()
-    popup.setHTML(d.content)
-    marker.setPopup(popup)
-
 })
